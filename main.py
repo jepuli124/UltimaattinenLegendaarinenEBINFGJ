56534a22d93f0f0a135759bb5132d2f97099ea77
Lauri Heiskanen <106094206+Lauri-Heiskanen@users.noreply.github.com>
2023-02-03 20:18:22 +0200

    not finished main.py

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+import pygame
+
+#älä muuta, kaikki kusee sitte
+MAX_FPS = 30 #frames per second
+CHARACTER_HEIGTH = 60 #pixels
+
+possibleActions = {"Up": moveUp(), "Down": moveDown(), "Left": moveLeft(), "Right": moveRight(), "Jump": jump(), "Quit": ()}
+
+running = True
+
+#main loop
+
+def main():
+    pressedKeys = []
+    PlayerVelocityX = 0.0
+    PlayerVelocityY = 0.0
+    while(running):
 
 
-print("Hello World!")
+        if(pygame.key.get_focused): #only do main loop while window focused
+            pressedKeys = getInput() #actions to perform
+            for action in pressedKeys:
+            pass    
+                
+            
+        else:
+            pass
+
+if(__name__ == "__main__"):
+    main()
