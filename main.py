40b8a82a54d20099188b2dbf8b08abe2fd297402
jepuli124 <konsta.jalkanen.kj@gmail.com>
2023-02-03 20:35:42 +0200

    update2

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import pygame
 pygame.init()
 
 Screen = [1920, 1080]
-
-
+Window = pygame.display.set_mode((Screen[0], Screen[1]))
 
 #älä muuta, kaikki kusee sitte
-MAX_FPS = 30 #frames per second
+ #frames per second
 CHARACTER_HEIGTH = 60 #pixels
 
 possibleActions = {"Up": moveUp(), "Down": moveDown(), "Left": moveLeft(), "Right": moveRight(), "Jump": jump(), "Quit": ()}
@@ -16,9 +15,13 @@ running = True
 #main loop
 
 def main():
+    clock = pygame.time.Clock()
+    MAX_FPS = 30
 
     while(running):
-
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
         if(pygame.key.get_focused): #only do main loop while window focused
             pressedKeys = getInput() #actions to perform
@@ -28,6 +31,7 @@ def main():
             
         else:
             pass
+        clock.tick(MAX_FPS)
 
 if(__name__ == "__main__"):
     main()
