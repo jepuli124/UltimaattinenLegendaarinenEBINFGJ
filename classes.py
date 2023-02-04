a8cb702d881422f98bae59efebb5d218474b8ac2
Lauri Heiskanen <106094206+Lauri-Heiskanen@users.noreply.github.com>
2023-02-04 21:39:28 +0200

    Level reader ja TILE luokka

--- classes.py
+++ classes.py
@@ -1,14 +1,59 @@
-class ENTITY():
-    def __init__(self, VelocityX, VelocityY, characterHeightX, characterHeightY, playerMovementAcceleration, playerPosX, playerPosY):
+class TILE:
+    def __init__(self, char, x, y):
+        if(char == 'a'):
+            self.sprite = pygame.transform.scale(os.path.join('Assets', 'Level1' 'dirtPlaceholder.png'), (WIDTH,HEIGHT))
+            self.solid = True
+            self.xCord = x
+            self.yCord = y
+        elif(char == 'b'):
+            self.sprite = pygame.transform.scale(os.path.join('Assets', 'Level1' '.png'), (WIDTH,HEIGHT))
+            self.solid = True
+            self.xCord = x
+            self.yCord = y
+
+class ENTITY:
+    def __init__(self, VelocityX, VelocityY, characterHeightX, characterHeightY, MovementAcceleration, PosX, PosY, sprite):
         self.VelocityX = VelocityX #pixels / tick
         self.VelocityY = VelocityY #pixels / tick
         self.characterHeightX = characterHeightX #pixels
         self.characterHeightY = characterHeightY #pixels
-        self.playerMovementAcceleration = playerMovementAcceleration #pixels / tick^2
-        self.playerPosX = playerPosX
-        self.playerPosY = playerPosY
-        self.jumpStregth = 4 #velocity given when jumpig
-        self.maxVelocity = 4 #pixels / tick
+        self.MovementAcceleration = MovementAcceleration #pixels / tick^2
+        self.PosX = PosX
+        self.PosY = PosY
+        self.sprite = sprite
+
+class PROJECTILE(ENTITY):
+    pass
+
+class PLAYER:
+    def __init__(self, VelocityX, VelocityY, characterHeightX, characterHeightY, MovementAcceleration, PosX, PosY, sprite):
+        self.VelocityX = VelocityX  # pixels / tick
+        self.VelocityY = VelocityY  # pixels / tick
+        self.characterHeightX = characterHeightX  # pixels
+        self.characterHeightY = characterHeightY  # pixels
+        self.MovementAcceleration = MovementAcceleration  # pixels / tick^2
+        self.PosX = PosX
+        self.PosY = PosY
+        self.jumpStregth = 4 #velocity given when jumping
+        self.sprite = sprite
 
 
+class NPC(ENTITY):
+    pass
 
+
+class MBG:
+    def __init__(self, VelocityX, VelocityY, characterHeightX, characterHeightY, PosX, PosY, sprite):
+        self.VelocityX = VelocityX  # pixels / tick
+        self.VelocityY = VelocityY  # pixels / tick
+        self.characterHeightX = characterHeightX  # pixels
+        self.characterHeightY = characterHeightY  # pixels
+        self.PosX = PosX
+        self.PosY = PosY
+        self.sprite = sprite
+
+    def destroy(self):
+        if self.PosX <= 0 or self.PosX >= 1920:
+            return 1
+        elif self.PosY < 0 or self.PosY >= 1080:
+            return 1
