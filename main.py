9fc244c42055ad3d6ff2f0965abd58724a23f8d0
jepuli124 <konsta.jalkanen.kj@gmail.com>
2023-02-05 14:16:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from functions import *
 from classes import * 
 from math import sqrt, ceil, floor
+import cv2
+import numpy as np
 
 pygame.init()
 
@@ -96,8 +98,40 @@ def main():
         clock.tick(MAX_FPS)
 
 
+def video():
+    cap = cv2.VideoCapture('./textures/EBIGLEGENDFJG.mp4')
+
+    # Check if camera opened successfully
+    if (cap.isOpened() == False):
+        print("Error opening video file")
+
+    # Read until video is completed
+    while (cap.isOpened()):
+
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret == True:
+            # Display the resulting frame
+            cv2.imshow('Frame', frame)
+
+            # Press Q on keyboard to exit
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+        # Break the loop
+        else:
+            break
+
+    # When everything done, release
+    # the video capture object
+    cap.release()
+
+    # Closes all the frames
+    cv2.destroyAllWindows()
+
 if(__name__ == "__main__"):
     startloop()
+    video()
     main()
 
 pygame.quit()
